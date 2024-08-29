4ece8871a95f534fdaf21f0c46d33e407c3a1dce
ROllerozxa <rollerozxa@voxelmanip.se>
2024-08-30 00:36:19 +0200

    Add RSS link on front page

--- build_production.sh
+++ build_production.sh
@@ -2,4 +2,6 @@
 
 rm -rf _site_production
 
-JEKYLL_ENV=production jekyll build -d ./_site_production
+JEKYLL_ENV=production bundle exec jekyll build -d ./_site_production
+
+rsync -azP --delete _site_production/ debian@voxelmanip.se:/srv/voxelmanip/
