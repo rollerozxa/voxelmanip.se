970582d093edea90987385615d70ae22d2827411
ROllerozxa <rollerozxa@voxelmanip.se>
2025-01-11 00:10:54 +0100

    Tweak and improve site styling

--- media/projects/gen-thumbs.sh
+++ media/projects/gen-thumbs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for file in *.webp; do
-	convert $file -resize 640 -unsharp 0x0.55+0.55+0.008 -quality 99% low/$file
+	magick $file -resize 640 -unsharp 0x0.55+0.55+0.008 -quality 99% low/$file
 done
